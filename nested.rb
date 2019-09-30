696e46b94fc933a92385f3298b0b4a4da4353eae
ZUBEYDE SISMAN <zubeydesisman@gmail.com>
2019-09-30 21:32:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -100,6 +100,8 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+         programmer_hash[:alan_kay][:known_for] = "GUI"
+
      alans_new_info = "GUI"
      
      
