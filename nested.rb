6533ea005be59d04fe025781a3fd68fd350e0596
ZUBEYDE SISMAN <zubeydesisman@gmail.com>
2019-09-30 21:34:34 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -77,13 +77,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-     
-programmer_hash[:yukihiro_matsumoto] = {
-                :known_for => "Ruby",
-                :languages => ["LISP", "C"]
-              }
-          
-  puts programmer_hash  
+     programmer_hash[:yukihiro_matsumoto] = {}
+		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+		 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+   programmer_hash  
 end
 
 def changing_alan
@@ -103,8 +100,10 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
+         programmer_hash[:alan_kay][:known_for] = "GUI"
+
+     #alans_new_info = "GUI"
+     programmer_hash
      
 end
 
@@ -124,5 +123,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+programmer_hash
 end
